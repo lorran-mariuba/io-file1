2e6b07527eff3cd4475217589f74dbb1a049178c
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-04-12 22:42:59 -0400

    Examples with FileWriter and BufferedWriter.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,14 +12,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		String path = "C:\\ProjetosEclipse\\projetos\\io-file1\\in.txt";
+		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String path = "C:\\ProjetosEclipse\\projetos\\io-file1\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
+
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		} 
