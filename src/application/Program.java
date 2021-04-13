5e0137aabd8360bc702e9e42b7be3ad3d617c539
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-04-12 22:31:26 -0400

    Example with FileReader and BufferedReader.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,25 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
+		String path = "C:\\ProjetosEclipse\\projetos\\io-file1\\in.txt";
 		
-		File file = new File("C:\\ProjetosEclipse\\projetos\\io-file1\\in.txt");
-		Scanner sc = null;
-		try {
-				sc = new Scanner(file);
-				while (sc.hasNextLine()) {
-					System.out.println(sc.nextLine());
-				}
-		}
-		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			if(sc != null) {				
-				sc.close();
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
-		}
-
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
 	}
-
 }
